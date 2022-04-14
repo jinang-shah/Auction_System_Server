44e8e1f42fe855dc5f473a50a0a7a25222cdb3eb
Jinang Shah <jinang.shah@solutionanalysts.in>
2022-04-14 19:45:13 +0530

    changes in model

--- model/user.js
+++ model/user.js
@@ -1,71 +1,94 @@
-const mongoose = require('mongoose')
-const jwt = require('jsonwebtoken')
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 const schema = mongoose.Schema;
 
-const User = new schema({
+const User = new schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        unique:true,
-        trim:true,
-        required: true,
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
     },
     mobile: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     address: {
-        type: {},
-        required: true
+      type: {},
+      required: true,
     },
     ducuments: {
-        type: []
+      type: [],
+    },
+    productBill: {
+      type: String,
     },
     isSeller: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-    buyHistory: {
-        type: [],
-        default:[]
+    buyHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    sellHistory: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    favouriteProdcts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+    notifications: {
+      type: [
+        {
+          productId: {
+            type: String,
+          },
+          when: {
+            type: String,
+          },
+        },
+      ],
     },
-    sellHistory: {
-        type: [],
-        default:[]
+    tokens: [
+      {
+        token: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-    },
-    favouriteProdcts: {
-        type: [],
-        default:[]
-    },
-    tokens:[{
-        token:{
-            type:String,
-            required:true
-        }
-    }]
-}, { timestamps: true })
+User.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, "dontcomehere");
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+  return token;
+};
 
-
- User.methods.generateAuthToken = async function() {
-     const user = this
-     const token= jwt.sign({_id:user._id.toString()}, 'dontcomehere')
-     user.tokens = user.tokens.concat({token}) 
-     await user.save()
-     return token;
- }
-
-
-module.exports = mongoose.model('Users', User)
+module.exports = mongoose.model("Users", User);
